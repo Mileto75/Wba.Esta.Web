a9f7017ecfc2b271a887616181c851513b26de0d
Miledi75 <mileto.di.marco@howest.be>
2023-10-19 15:32:46 +0200

    Add project files.

---
+++ Wba.Esta.Web/Controllers/EstaController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Wba.Esta.Web.ViewModels;
+
+namespace Wba.Esta.Web.Controllers
+{
+    public class EstaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Apply()
+        {
+            //init the form
+            var estaApplyViewModel = new EstaApplyViewModel();
+            estaApplyViewModel.Arrival = DateTime.Now.Date;
+            //countries
+            estaApplyViewModel.Countries
+                = new List<SelectListItem>
+                {
+                    new SelectListItem{Text = "België", Value ="1"},
+                    new SelectListItem{Text = "Nederland", Value ="2"},
+                };
+
+            return View(estaApplyViewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Process(EstaApplyViewModel estaApplyViewModel)
+        {
+            //check for errors
+            if (!ModelState.IsValid)
+            {
+                estaApplyViewModel.Countries
+                = new List<SelectListItem>
+                {
+                    new SelectListItem{Text = "België", Value ="1"},
+                    new SelectListItem{Text = "Nederland", Value ="2"},
+                };
+                return View("Apply",estaApplyViewModel);
+            }
+            //custom errors
+            //date cannot be in the past
+            if(estaApplyViewModel.Arrival.Date < DateTime.Now.Date)
+            {
+                ModelState.AddModelError("Arrival", "Date must be in the future");
+                estaApplyViewModel.Countries
+                = new List<SelectListItem>
+                {
+                    new SelectListItem{Text = "België", Value ="1"},
+                    new SelectListItem{Text = "Nederland", Value ="2"},
+                };
+                return View("Apply",estaApplyViewModel);
+            }
+            //no more errors
+            // post redirect get principe
+            return RedirectToAction("Registered");
+        }
+        public IActionResult Registered()
+        {
+            return View();
+        }
+    }
+}
---
+++ Wba.Esta.Web/ViewModels/EstaApplyViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Wba.Esta.Web.ViewModels
+{
+    public class EstaApplyViewModel
+    {
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Aankomstdatum verplicht!")]
+        [Display(Name = "Aankomstdatum")]
+        public DateTime Arrival { get; set; }
+        [DataType(DataType.EmailAddress, ErrorMessage = "Geef een correct emailadres in!")]
+        [Required(ErrorMessage = "Email verplicht!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Telefoon verplicht!")]
+        [DataType(DataType.PhoneNumber,ErrorMessage = "Graag correct formaat!")]
+        [Display(Name = "Tel")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Postcode verplicht!")]
+        [Display(Name = "Postcode")]
+        public string PostalCode { get; set; }
+        [Display(Name = "Straat en huisnummer")]
+        
+        [Required(ErrorMessage = "Straat en huisnummer verplicht!")]
+        public string StreetAndNumber { get; set; }
+        
+        [Display(Name = "Stad")]
+
+        [Required(ErrorMessage = "Woonplaats verplicht!")]
+        public string Municipality { get; set; }
+        [Display(Name = "Provincie")]
+
+        [Required(ErrorMessage = "Provincie verplicht!")]
+        
+        public string Province { get; set; }
+        //list of countries
+        [Display(Name = "Land")]
+        public int SelectedCountryId { get; set; }
+        public IEnumerable<SelectListItem> Countries { get; set; }
+
+        [Display(Name = "Ook post?")]
+        public bool ByMail { get; set; }
+    }
+}
