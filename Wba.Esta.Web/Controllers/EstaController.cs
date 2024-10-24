34e4a7af22ff78083c0568dae50ad0f9b69bf5f8
Mileto Di Marco <mileto.di.marco@howest.be>
2024-10-24 09:48:20 +0200

    Add esta form

--- Wba.Esta.Web/Controllers/EstaController.cs
+++ Wba.Esta.Web/Controllers/EstaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Wba.Esta.Web.ViewModels;
 
 namespace Wba.Esta.Web.Controllers
 {
@@ -7,12 +9,42 @@ namespace Wba.Esta.Web.Controllers
         [HttpGet]
         public IActionResult Apply()
         {
-            return View();
+            //create the viewmodel
+            //fill the countries List
+            var estaApplyViewModel = new EstaApplyViewModel();
+            estaApplyViewModel.Countries
+                = new List<SelectListItem> 
+                { 
+                    new SelectListItem{Value="0",Text="Choose a country",Selected=true,Disabled=true},
+                    new SelectListItem{Value="1",Text="België" },
+                    new SelectListItem{Value="2",Text="Nederland" },
+                    new SelectListItem{Value="3",Text="Luxemburg" },
+                };
+            estaApplyViewModel.Arrivaldate = DateTime.Now;
+            return View(estaApplyViewModel);
         }
         [HttpPost]
-        public IActionResult Process()
+        public IActionResult Process(EstaApplyViewModel estaApplyViewModel)
         {
-            return View("Apply");
+            //test the date
+            if(estaApplyViewModel.Arrivaldate <= DateTime.Now)
+            {
+                //add model error
+                ModelState.AddModelError("ArrivalDate", "Must be in the future!");
+            }
+            if(!ModelState.IsValid)
+            {
+                estaApplyViewModel.Countries
+                = new List<SelectListItem>
+                {
+                    new SelectListItem{Value="0",Text="Choose a country",Selected=true,Disabled=true},
+                    new SelectListItem{Value="1",Text="België" },
+                    new SelectListItem{Value="2",Text="Nederland" },
+                    new SelectListItem{Value="3",Text="Luxemburg" },
+                };
+                return View("Apply",estaApplyViewModel);
+            }
+            return RedirectToAction("Succes","Esta");
         }
         [HttpGet]
         public IActionResult Succes()
